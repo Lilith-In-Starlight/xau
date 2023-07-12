a73e97a0b073ce79fa71a6b28390733de7e8b4d9
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-07-12 12:00:27 -0500

    Organized the new nodes

--- areas/area.gd
+++ areas/area.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var first_section :NodePath
 @onready var first_section_node : Node2D = get_node_or_null(first_section)
 @onready var player_node: CharacterBody2D = get_tree().get_nodes_in_group("Player")[0]
+
+
 func _ready() -> void:
 	for i in get_children():
 		if SaveData.data["sections"].has(str(i.get_path())):
