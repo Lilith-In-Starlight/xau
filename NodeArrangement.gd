bafd12a62400c9f5558459d16dee1467eed5b1f6
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-07-21 13:58:52 -0500

    Revert "Godot insisted on this one"
    This reverts commit f8081d6ddc27723b7a883c278719fda3f19b281c.

--- NodeArrangement.gd
+++ NodeArrangement.gd
@@ -3,6 +3,7 @@ extends Resource
 
 class_name NodeArrangement
 
+@export var thing := Rect2()
 
 func _init():
 	resource_local_to_scene = true
@@ -31,4 +32,5 @@ func get_used_area(puzzle: Puzzle) -> Rect2:
 		if child is Node2D and not child.name == "NoNode":
 			area = area.expand(child.position)
 	
+	thing = area
 	return area
