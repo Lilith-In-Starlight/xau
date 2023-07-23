2216b49b90a57e701a8f0a3358169cd94e74608e
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-07-22 23:31:59 -0500

    Added new tree species

--- TreeDecoration.gd
+++ TreeDecoration.gd
@@ -1,29 +1,42 @@
 @tool
 extends StaticBody2D
 
+enum COLOR {
+	ORANGE,
+	BLUE
+}
 
-const TREE_SPRITES := [
-	preload("res://sprites/misc/trees/orange/puzzle_1.png"),
-	preload("res://sprites/misc/trees/orange/puzzle_2.png"),
-	preload("res://sprites/misc/trees/orange/puzzle_3.png"),
-	preload("res://sprites/misc/trees/orange/puzzle_4.png"),
-	preload("res://sprites/misc/trees/orange/puzzle_5.png"),
-	preload("res://sprites/misc/trees/orange/tree_6.png"),
-	preload("res://sprites/misc/trees/orange/tree_7.png"),
-	preload("res://sprites/misc/trees/orange/tree_8.png"),
-	preload("res://sprites/misc/trees/orange/tree_9.png"),
-	preload("res://sprites/misc/trees/orange/tree_10.png"),
-	preload("res://sprites/misc/trees/orange/tree_11.png"),
-	preload("res://sprites/misc/trees/orange/tree_12.png"),
-	preload("res://sprites/misc/trees/orange/tree_13.png"),
-	preload("res://sprites/misc/trees/orange/tree_14.png"),
-	preload("res://sprites/misc/trees/orange/tree_15.png"),
-	preload("res://sprites/misc/trees/orange/tree_16.png"),
-	preload("res://sprites/misc/trees/orange/tree_17.png"),
-]
+const TREE_SPRITES := {
+		COLOR.ORANGE: [
+			preload("res://sprites/misc/trees/orange/puzzle_1.png"),
+			preload("res://sprites/misc/trees/orange/puzzle_2.png"),
+			preload("res://sprites/misc/trees/orange/puzzle_3.png"),
+			preload("res://sprites/misc/trees/orange/puzzle_4.png"),
+			preload("res://sprites/misc/trees/orange/puzzle_5.png"),
+			preload("res://sprites/misc/trees/orange/tree_6.png"),
+			preload("res://sprites/misc/trees/orange/tree_7.png"),
+			preload("res://sprites/misc/trees/orange/tree_8.png"),
+			preload("res://sprites/misc/trees/orange/tree_9.png"),
+			preload("res://sprites/misc/trees/orange/tree_10.png"),
+			preload("res://sprites/misc/trees/orange/tree_11.png"),
+			preload("res://sprites/misc/trees/orange/tree_12.png"),
+			preload("res://sprites/misc/trees/orange/tree_13.png"),
+			preload("res://sprites/misc/trees/orange/tree_14.png"),
+			preload("res://sprites/misc/trees/orange/tree_15.png"),
+			preload("res://sprites/misc/trees/orange/tree_16.png"),
+			preload("res://sprites/misc/trees/orange/tree_17.png"),
+		],
+		COLOR.BLUE: [
+			preload("res://sprites/misc/trees/blue/tree_1.png"),
+			preload("res://sprites/misc/trees/blue/tree_2.png"),
+			preload("res://sprites/misc/trees/blue/tree_3.png"),
+		],
+	}
+
+@export var species: COLOR = COLOR.ORANGE
 
 func _ready() -> void:
-	$Sprite2D.texture = TREE_SPRITES[hash(global_position)%TREE_SPRITES.size()]
+	$Sprite2D.texture = TREE_SPRITES[species][hash(global_position)%TREE_SPRITES[species].size()]
 	$Sprite2D.offset.y = - $Sprite2D.texture.get_height() / 2.0 + 5.0
 	$Sprite2D/VisibleOnScreenEnabler2D.rect.position.y = - $Sprite2D.texture.get_height()
 	$Sprite2D.flip_h = bool(hash(global_position)%2)
@@ -31,6 +44,7 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if Engine.is_editor_hint():
-		$Sprite2D.texture = TREE_SPRITES[hash(global_position)%TREE_SPRITES.size()]
+		$Sprite2D.texture = TREE_SPRITES[species][hash(global_position)%TREE_SPRITES[species].size()]
 		$Sprite2D.offset.y = - $Sprite2D.texture.get_height() / 2.0 + 5.0
 		$Sprite2D/VisibleOnScreenEnabler2D.rect.position.y = - $Sprite2D.texture.get_height()
+		$Sprite2D.flip_h = bool(hash(global_position)%2)
