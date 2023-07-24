847f8c806f793d52a624f20e4e45bc33fc4d586e
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-07-23 22:10:49 -0500

    Fix the archer puzzle

--- HardcodeNodeRule.gd
+++ HardcodeNodeRule.gd
@@ -2,13 +2,17 @@ extends NodeRule
 
 class_name HardcodeNodeRule
 
-@export var hardcoded_connections :Array[PuzzleNode]
+@export var hardcoded_connections :Array[NodePath]
 
 
 func check_correctness(local_node: PuzzleNode) -> bool:
+	print(local_node.name)
+	print(local_node.connections)
+	print(hardcoded_connections)
 	if local_node.connections.size() != hardcoded_connections.size():
 		return false
 	for hardcoded_node in hardcoded_connections:
-		if not hardcoded_node in local_node.connections:
+		if not local_node.get_node(hardcoded_node) in local_node.connections:
+			print(local_node.get_node(hardcoded_node))
 			return false
 	return true
