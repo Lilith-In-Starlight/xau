1d47e422e5bb024bb83eeb00546ad8ec37ba02d9
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-07-26 19:43:31 -0500

    Made the MusicHandler code more readable

--- MusicHandler.gd
+++ MusicHandler.gd
@@ -37,30 +37,15 @@ func _process(_delta: float) -> void:
 		$HomeGuitar.play()
 
 
-	first_nexus_droning.volume = -60
-	forest_pam_flute.volume = -60
-	forest_little_bell.volume = -60
-	forest_choir_bass.volume = -60
-	first_nexus_droning.volume = -60.0
+	for channel in MusicChannel.all_music_channels:
+		channel.volume = -60.0
 
+	manage_single_events()
 	match get_parent().current_area:
 		"first_nexus":
 			match StatesDefiner.state:
 				"outside":
 					first_nexus_droning.volume = 0.0
-					if SaveData.save_handler.get_value("first_exit", true):
-						SaveData.save_handler.save_value("first_exit", false)
-						$WeirdSound.stream = preload("res://music/weird_sound_once.mp3")
-						$WeirdSound.play()
-						var tween = create_tween()
-						tween.set_ease(Tween.EASE_OUT)
-						tween.set_trans(Tween.TRANS_QUINT)
-						tween.tween_property($"../Camera2D", "zoom", Vector2(1/5.0, 1/5.0), 2.0)
-						tween.tween_property($"../Camera2D", "zoom", Vector2(1/5.0, 1/5.0), 2.0)
-						tween.set_ease(Tween.EASE_IN_OUT)
-						tween.set_trans(Tween.TRANS_QUAD)
-						tween.tween_property($"../Camera2D", "zoom", Vector2(1, 1), 7.0)
-
 					if player.position.x < -500:
 						forest_pam_flute.volume = -10
 						forest_little_bell.volume = -5
@@ -68,11 +53,11 @@ func _process(_delta: float) -> void:
 						forest_choir_bass.volume = -10
 
 				"exit_hall":
-					if SaveData.save_handler.get_value("first_exit", true):
-						if player.position.y < 560:
-							first_nexus_droning.volume = 0.0
-						else:
-							first_nexus_droning.volume = (player.position.y - 560) / 560.0 * 11.0
+					var player_has_not_exited_house :bool = SaveData.save_handler.get_value("first_exit", true)
+					if player_has_not_exited_house:
+						first_nexus_droning.volume = fit_in_range(player.position.y, 538.0, 1244.0, 0.0, 12.0)
+					else:
+						first_nexus_droning.volume = 0.0
 				_:
 					first_nexus_droning.volume = 0.0
 		"forest":
@@ -85,6 +70,37 @@ func _process(_delta: float) -> void:
 	for channel in MusicChannel.all_music_channels:
 		channel.adjust_volume()
 
+
 func play_tutorial_guitar():
 	$HomeGuitar.stream = preload("res://music/house_guitar_once.mp3")
 	$HomeGuitar.play()
+
+
+func manage_single_events():
+	var player_has_not_left_house :bool = SaveData.save_handler.get_value("first_exit", true)
+	if get_parent().current_area == "first_nexus" and StatesDefiner.state == "outside" and player_has_not_left_house:
+		SaveData.save_handler.save_value("first_exit", false)
+		$WeirdSound.stream = preload("res://music/weird_sound_once.mp3")
+		$WeirdSound.play()
+		var tween = create_tween()
+		tween.set_ease(Tween.EASE_OUT)
+		tween.set_trans(Tween.TRANS_QUAD)
+		tween.tween_property($"../Camera2D", "zoom", Vector2(1/5.0, 1/5.0), 1.0)
+		tween.tween_property($"../Camera2D", "zoom", Vector2(1/5.0, 1/5.0), 3.0)
+		tween.set_ease(Tween.EASE_IN_OUT)
+		tween.set_trans(Tween.TRANS_QUAD)
+		tween.tween_property($"../Camera2D", "zoom", Vector2(1, 1), 7.0)
+
+
+func fit_in_range(input: float, min_input: float, max_input: float, min_output: float, max_output: float) -> float:
+	if input < min_input:
+		return min_output
+	elif input > max_input:
+		return max_output
+	else:
+		var output_range := max_output - min_output
+		var input_range := min_input - max_input
+
+		var input_in_range := (input - min_input)
+		var fraction_of_input_in_range :float = abs(input_in_range / input_range)
+		return fraction_of_input_in_range * output_range + min_output
