38641da0c06b48ae3cd0e0334be0f2ea726fac3b
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-07-26 19:44:47 -0500

    Made branches explicitly reject having two colors

--- BranchLenghtNodeRule.gd
+++ BranchLenghtNodeRule.gd
@@ -12,21 +12,24 @@ func check_correctness(local_node: PuzzleNode) -> bool:
 	var branch :Array[PuzzleNode] = local_node.get_branch()
 	var collective_required_length := required_length
 	var endpoints : Array[PuzzleNode] = []
-	
+
 	for node in branch:
 		if node.connections.size() != 2:
 			endpoints.append(node)
-		
+
 		if node == local_node:
 			continue
-		
-		if node.node_rule is BranchLengthNodeRule and node.node_rule.color == color:
-			collective_required_length += node.node_rule.required_length
-	
+
+		if node.node_rule is BranchLengthNodeRule:
+			if node.node_rule.color == color:
+				collective_required_length += node.node_rule.required_length
+			else:
+				return false
+
 	if branch.size() < 2:
 		return false
-	
+
 	if endpoints.size() != 2:
 		return branch.size() == collective_required_length
-	
+
 	return branch.size() - 1 == collective_required_length
