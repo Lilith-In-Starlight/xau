095c41024bb83b3f52caac45eba5c6fe70ed7ad7
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2023-07-29 00:36:22 -0500

    Added the last condition of the CycleNodeRule

--- CycleNodeRule.gd
+++ CycleNodeRule.gd
@@ -7,4 +7,14 @@ func _init():
 	resource_local_to_scene = true
 
 func check_correctness(local_node: PuzzleNode) -> bool:
-	return not local_node.get_closest_loop().is_empty()
+	var closest_loop := local_node.get_closest_loop()
+
+	if closest_loop.is_empty():
+		return false
+
+	for node in closest_loop:
+		if node == local_node:
+			continue
+		elif node.node_rule is CycleNodeRule and node.node_rule.color == color:
+			return false
+	return true
